fff2be69e058317209bc6494b667b49a5498f075
Danazone04 <minhthuvlt@gmail.com>
2018-09-17 15:56:51 +0700

    Initial commit

---
+++ app/src/main/java/com/thu/danazone04/danavideo/common/Common.java
@@ -0,0 +1,6 @@
+package com.thu.danazone04.danavideo.common;
+
+public class Common {
+    public static final String URL_REGISTER = "http://huyhoangdanang.com/danazone/register_photo.php";
+    public static final String URL_LOGIN = "http://huyhoangdanang.com/danazone/login_photo.php";
+}
